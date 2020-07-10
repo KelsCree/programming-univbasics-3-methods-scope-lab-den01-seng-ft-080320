0eec5f29263cfeb9fc17c9fd6e9b12d1d4ee010b
Kelsey Creehan <kelseycreehan@gmail.com>
2020-07-10 21:26:58 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,20 @@
 def mario
   phrase = "It's-a me, Mario!"
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  output = "It's Dangerous To Go Alone! Take This."
+  puts output
+end
+
+def all_phrases
+  mario
+  toadstool
+  link
+  
